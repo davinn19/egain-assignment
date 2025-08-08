ef28391d772a88947a4d5150a35ff125f833b1c4
btthl <davin.nguyen19@gmail.com>
2025-08-07 17:16:20 -0700

    added recommendation logic

--- main.py
+++ main.py
@@ -78,24 +78,46 @@ class OptionsDialogNode(DialogNode):
         number_input = int(user_input)
         return number_input >= 1 and number_input <= len(self.options)
     
+    # Returns input in number form
+    def execute(self):
+        return int(super().execute())
     pass
 
+class Laptop:
+    def __init__(self, name: str, price: int, processing_tier: int, battery_life: int, storage: int, screen_tier: int):
+        self.name = name
+        self.price = price
+        self.processing_tier = processing_tier
+        self.battery_life = battery_life
+        self.storage = storage
+        self.screen_tier = screen_tier
+        pass
+
+
 if __name__ == "__main__":
+    # Defining different laptop models
+    laptops = [
+        Laptop("CheapBook", 299, 1, 5, 256, 1),
+        Laptop("Bell XYZ 13", 575, 2, 10, 750, 2),
+        Laptop("HD Ghost 2 in 1", 999, 3, 8, 512, 3),
+        Laptop("Mono Victis 15", 799, 4, 3, 2000, 4),
+        Laptop("R7 Pro Super Max", 4500, 5, 18, 1000, 5)
+    ]
+
     # Defining the different dialog nodes
     intro_node = ConfirmDialogNode(
-        "Hi! I'm Davin, a chatbot designed to find your perfect laptop. Would you like my assistance"
+        "Hi! I'm Davin, a chatbot designed to find your perfect laptop. Would you like my assistance?"
     )
 
     budget_node = OptionsDialogNode(
         "What is your budget?",
-        ["0-250", "250-500", "500-750", "750-1000", "1000+"],
+        ["$0-$250", "$250-$500", "$500-$750", "$750-$1000", "$1000+"],
     )
 
     use_case_node = OptionsDialogNode(
         "Which of the following best describes what you will use the laptop for?",
-        ["Office Work", "Web Surfing", "Gaming", "Video Editing", "CAD"]
+        ["Office Work", "Web Surfing", "Gaming", "Video Editing"]
     )
-
 
     # Start the conversation
     user_needs_help = intro_node.execute()
@@ -105,5 +127,38 @@ if __name__ == "__main__":
     
     budget = budget_node.execute()
     use_case = use_case_node.execute()
+
+    # Find best laptop matching criteria
+    best_laptop: Laptop = None
+    best_laptop_score = 0
+
+    max_price = 250 * budget
+    if max_price > 1000:
+        max_price = -1
+
+    for laptop in laptops:
+        # Price must be within budget
+        if max_price != -1 and laptop.price > max_price:
+            continue
+        
+        # Use laptop specs for score
+        score = 0
+        if use_case == 1:
+            score = laptop.weight
+        elif use_case == 2:
+            score = laptop.price
+        elif use_case == 3:
+            score = laptop.processing_tier
+        else:
+            score = laptop.storage
+        
+        if score > best_laptop_score:
+            best_laptop = laptop
+            best_laptop_score = score
+
+    if best_laptop == None:
+        print("Sorry, we could not find a laptop within your budget.")
+    else:
+        print("Based on your responses, I recommend the {}.".format(best_laptop.name))
 
     pass
